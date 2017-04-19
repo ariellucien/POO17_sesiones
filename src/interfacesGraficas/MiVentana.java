c3ce97c0a74839b858eee5143e4f2c1b9d999d19
administrador1 <administrador1@LS_11>
2017-04-19 13:26:46 -0500

    JFrames

---
+++ src/interfacesGraficas/MiVentana.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesGraficas;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author administrador1
+ */
+public class MiVentana extends JFrame {
+    public static void main(String[] args) {
+        MiVentana mv = new MiVentana();
+        mv.setSize(500, 500);
+        mv.setVisible(true);
+    }
+}
